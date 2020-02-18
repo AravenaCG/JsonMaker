49d00755ccf4c13638ab0fbaa4dbd01dddf9f3c1
Cristian Aravena <caravena@exisoft.com.ar>
2020-02-18 20:17:37 -0300

    Primer commit : proyecto funcional, todas las rows de la tabla en un solo JSON, TODO: escribir un .json por cada row de la tabla

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace JsonMaker
+{
+    partial class JsonMForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.chkElementsBox = new System.Windows.Forms.CheckedListBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnToJSON = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(70, 19);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(77, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "File Path (.csv)";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(191, 19);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(611, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(808, 19);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(103, 20);
+            this.btnReadFile.TabIndex = 3;
+            this.btnReadFile.Text = "Read File!";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // chkElementsBox
+            // 
+            this.chkElementsBox.FormattingEnabled = true;
+            this.chkElementsBox.Location = new System.Drawing.Point(191, 45);
+            this.chkElementsBox.Name = "chkElementsBox";
+            this.chkElementsBox.Size = new System.Drawing.Size(611, 79);
+            this.chkElementsBox.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(808, 45);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(103, 79);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate Files";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(178, 260);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(0, 13);
+            this.lblTest.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(73, 334);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(884, 218);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(808, 130);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(103, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(917, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 293);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1113, 669);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnToJSON
+            // 
+            this.btnToJSON.Location = new System.Drawing.Point(808, 160);
+            this.btnToJSON.Name = "btnToJSON";
+            this.btnToJSON.Size = new System.Drawing.Size(103, 33);
+            this.btnToJSON.TabIndex = 13;
+            this.btnToJSON.Text = "ToJSON";
+            this.btnToJSON.UseVisualStyleBackColor = true;
+            this.btnToJSON.Click += new System.EventHandler(this.btnToJSON_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(608, 160);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(194, 152);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // JsonMForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 681);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnToJSON);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.chkElementsBox);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "JsonMForm";
+            this.Text = "JsonMaker";
+            this.Load += new System.EventHandler(this.JsonMForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.CheckedListBox chkElementsBox;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnToJSON;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonMaker
+{
+    public partial class JsonMForm : Form
+    {
+        const char separator = ',';
+        List<string> elements = new List<string>();
+        List<int> pos = new List<int>();
+        int numRows = 0;
+        DataTable finalTable = new DataTable();
+        
+     
+        public JsonMForm()
+        {
+            InitializeComponent();
+        }
+
+        private void JsonMForm_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"C:\Users\caravena\source\repos\JsonMaker\tenor.gif");
+            pictureBox2.Image = Image.FromFile(@"C:\Users\caravena\source\repos\JsonMaker\TemptingShyFireant-size_restricted.gif");
+            pictureBox3.Image = Image.FromFile(@"C:\Users\caravena\source\repos\JsonMaker\tenor2.gif");
+            pictureBox3.Visible = false;
+           // pictureBox2.Size.
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            string path = txtFilePath.Text;
+            const char separator = ',';
+            chkElementsBox.Items.Clear();
+            using (var reader = new StreamReader(path))
+            {
+                string firstLine = reader.ReadLine();
+
+                //lblPath.Text = firstLine;
+
+                foreach (var element in firstLine.Split(separator))
+                {
+                    elements.Add(element);
+                }
+
+                //listBox1.DataSource = elements;
+                foreach (var item in elements)
+                {
+                    chkElementsBox.Items.Add(item);
+                }
+
+            }
+            dataGridView1.DataSource = ReadDataTable(txtFilePath.Text);
+        }
+
+        public string DataTableToJSONWithJSONNet(DataTable table)
+        {
+            string JSONString = string.Empty;
+            JSONString =  JsonConvert.SerializeObject(table);
+            return JSONString;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            //agrego la posicion de cada item seleccionado en una lista
+            foreach (string itemChecked in chkElementsBox.CheckedItems)
+            {
+                pos.Add(chkElementsBox.Items.IndexOf(itemChecked));
+            }
+            
+            finalTable = WriteDataTable(txtFilePath.Text, pos);
+            dataGridView1.DataSource = finalTable;
+
+
+        }
+
+        public DataTable ReadDataTable(string filePath)
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+
+                DataTable dataTable = new DataTable();
+
+                string[] headers = sr.ReadLine().Split(separator);
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(separator);
+                    DataRow dr = dataTable.NewRow();
+                    numRows=numRows+1;
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    dataTable.Rows.Add(dr);
+                }
+                return dataTable;
+
+            }
+
+        }
+
+
+
+        public DataTable WriteDataTable(string filePath, List<int> colums)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            int t = 0;
+            DataTable dataTable = new DataTable();
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+
+                foreach(string item in chkElementsBox.CheckedItems)
+                {
+                    dataTable.Columns.Add(item);
+                }
+                
+                while(!sr.EndOfStream)
+                {
+                    DataRow dr = dataTable.NewRow();
+                    string[] rows = sr.ReadLine().Split(separator);
+                    if (t > 0)
+                    {
+       
+                        for (int i = 0; i < colums.Count; i++)
+                        {
+                            dr[i] = rows[colums[i]];
+
+
+                        }
+                        dataTable.Rows.Add(dr);
+                    }
+                    t++;
+                }
+                
+            }
+
+            return dataTable;
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFilePath.Text = null;
+            pictureBox3.Visible = false;
+             elements = new List<string>();
+             pos = new List<int>();
+
+            numRows = 0;
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            chkElementsBox.Items.Clear();
+
+        }
+
+        private void btnToJSON_Click(object sender, EventArgs e)
+        {
+            string jsonString = DataTableToJSONWithJSONNet(finalTable);
+            string pathExit = @"C:\Users\caravena\source\repos\JsonMaker\Json.json";
+            File.WriteAllText(pathExit, jsonString);
+            pictureBox3.Visible = true;
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
